88f0d9223e2cb2bc0fe491cb663ccaa1a176aa35
choongrae <choongraej@gmail.com>
2024-01-13 19:35:24 +0900

    Design: Add typography

---
+++ app/src/styles/typography.ts
@@ -0,0 +1,7 @@
+import {ReactNativeStyle, css} from '@emotion/native';
+
+export const typography: Record<TypoToken, ReactNativeStyle> = {
+  headline1: css``,
+  subtitle1: css``,
+  body1: css``,
+};
