f351a601ca48bc8e53c89a768dbd7b961389fa41
choongrae <choongraej@gmail.com>
2024-01-13 18:35:13 +0900

    init

---
+++ app/babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          'ios.js',
+          'android.js',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+};
